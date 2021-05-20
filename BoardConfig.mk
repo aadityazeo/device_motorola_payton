44cfab8ef5c0ebe304cc6d0401caf61827892715
cool585 <dhineshcool585@gmail.com>
2021-05-20 11:14:57 +0530

    payton:update defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ BOARD_HAVE_QCOM_FM := true
 BOARD_HAS_QCA_FM_SOC := "cherokee"
 
 # Kernel
-TARGET_KERNEL_CONFIG := cybertron_defconfig
+TARGET_KERNEL_CONFIG := cybertron_payton_defconfig
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/touch.xml
